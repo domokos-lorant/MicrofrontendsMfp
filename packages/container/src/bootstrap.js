ea131d32a79ded40125d19f79c34ddcd7174534c
Lorant Domokos <domokos.lorant@yahoo.com>
2021-05-23 11:31:40 -0700

    Add publicPath

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-// Test change to force a build.
 ReactDOM.render(<App />, document.querySelector("#root"));
