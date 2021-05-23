71ffb32cd24ab25e0b864103578e4f4f3ab5152f
Lorant Domokos <domokos.lorant@yahoo.com>
2021-05-23 11:21:45 -0700

    Test change

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Test change to force a build.
 ReactDOM.render(<App />, document.querySelector("#root"));
